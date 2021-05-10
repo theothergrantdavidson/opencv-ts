36baab7a6a3b7bcb7c5df7813e80ee5a81432a30
Grant Davidson <grant.l.davidson@gmail.com>
2021-05-10 10:49:34 +1000

    Remove non-existent member functions

--- src/core/Range.d.ts
+++ src/core/Range.d.ts
@@ -2,9 +2,6 @@ declare module Range {
     interface Range {
         new(): Range;
         new(start: number, end: number): Range;
-        size(): number;
-        empty(): boolean;
-        all(): Range;
         start: number,
         end: number
     }
