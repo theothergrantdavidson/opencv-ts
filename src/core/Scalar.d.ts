563a90fa8f540af733aac5852239253f3b99ea2d
Rafael Alexandre Piemontez <rafaelpiemontez@gmail.com>
2022-04-28 20:42:45 -0300

    Add static method all
    Tested according below docs and worked.
    https://docs.opencv.org/3.4/d1/da0/classcv_1_1Scalar__.html#ac1509a4b8454fe7fe29db069e13a2e6f

--- src/core/Scalar.d.ts
+++ src/core/Scalar.d.ts
@@ -3,6 +3,9 @@ declare module  Scalar {
         new(): Scalar;
         new(s1: number): Scalar;
         new(s1: number, s2: number, s3: number, s4?: number): Scalar;
+
+        //returns a scalar with all elements set to v0
+        all(v0: number): Scalar;
     }
 }
 export = Scalar
